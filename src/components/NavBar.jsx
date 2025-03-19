e2b9a7592f56ef8cf1e9e31a60f390b97263d537
Riccardo Moncada <rmondev@gmail.com>
2025-03-18 22:30:50 -0400

    fiddling with style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,11 +22,21 @@ const NavBar = () => {
         console.log(error)
       }
     };
+
+
+    // button {
+    //   border-color: black;
+    //   border-width: 2px;
+    //   color: black;
+    //   width: fit-content;
+    //   padding: 8px;
+    // }
+
   
   return (
     <nav className='flex flex-row p-4 justify-between'>
       <Link className='cursor-pointer' href='/'>
-        <h1 className='text-4xl'>CucinAI</h1>
+        <h1 className='xl:text-[48px] lg:text-[24px] md:text-[32px]'>CucinAI</h1>
       </Link>
 
       {user && 
@@ -34,26 +44,22 @@ const NavBar = () => {
           <section className='flex flex-row justify-between'>
       
             <Link href='/recipes'>
-              <button className='ml-4 cursor-pointer'>Recipes</button> 
+              <button className='cursor-pointer md:text-lg lg:text-xl w-fit border-2 border-black p-1'>Recipes</button> 
             </Link>
             <Link href='/addRecipe'>
-              <button className='ml-4 cursor-pointer'>Add a Recipe</button> 
+              <button className='ml-4 cursor-pointer md:text-lg lg:text-xl w-fit border-2 border-black p-1'>Add Recipe</button> 
             </Link>
           </section>
         </>
         }
 
-      {!user ? 
-        <button className='cursor-pointer'
-
-          onClick={handleSignIn}
-          >Sign In
-          </button>
-        :
-        <button className='cursor-pointer'
-          onClick={handleSignOut}
-          >Log Out</button>
-      }
+    
+        <button 
+          className='cursor-pointer md:text-lg lg:text-xl w-fit border-2 border-black p-1'
+          onClick={!user ? handleSignIn : handleSignOut}>
+            {!user ? 'Sign In' : 'Log Out'}
+        </button>
+       
       
     </nav>
   )
