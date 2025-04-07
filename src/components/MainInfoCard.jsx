c3eec82107f426d0d7e21904dfe21ed97ea472a0
Riccardo Moncada <rmondev@gmail.com>
2025-04-07 01:25:12 -0400

    added a MainInfoCard to start developing main page info section

--- src/components/MainInfoCard.jsx
+++ src/components/MainInfoCard.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import AIChefMain from '@/assets/home_images/aichef_main.png'
+import Image from 'next/image'
 
 const MainInfoCard = () => {
   return (
     <main className='
         relative
-        border-2
+        border-4
         border-black
         rounded-3xl
         z-20
         bg-transparent
         w-11/12
-        h-fit
+        h-11/12
+        p-8
         '>
-            <h1> Hello </h1>
+        <div className='flex flex-row gap-2'>
+            <div className='w-1/2 border-2 rounded border-black'>
+            <Image
+                src={AIChefMain}
+                width={320}
+                height={400}
+                alt='ai-chef-main'
+
+            />
+            </div>
+
+            <div className='border-2 rounded border-black w-1/2 bg-white'>
+
+            </div>
+        </div>
+
     </main>
   )
 }
